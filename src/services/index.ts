4bb1cea3bd833fd90baee7ba0aab60ee36f16679
Hiroshi Ando <hir-ando@sios.com>
2024-04-12 13:19:22 +0900

    check linter

--- src/services/index.ts
+++ src/services/index.ts
@@ -20,6 +20,8 @@ export function pageToOffset(page: number = 1, localLimit = limit): { limit: num
   return { limit: localLimit, offset }
 }
 
-export function isFetchError<E = GenericErrorModel>(e: unknown): e is HttpResponse<unknown, E> {
+export
+function
+isFetchError<E = GenericErrorModel>(e: unknown): e is HttpResponse<unknown, E> {
   return e instanceof Object && 'error' in e
 }
